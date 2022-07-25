0e6dc84333ecc588d06615679f1d258cb49ebf82
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2022-07-25 15:00:30 +0630

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Nyi Ye Han on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
